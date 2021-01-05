2920783588142a6cb07b2df5923facd24da58ac9
lijianhua <lijianhua@duia.com>
2021-01-05 14:24:57 +0800

    Initial commit

---
+++ src/main/groovy/com/pysun/flutter/MavenExtension.groovy
@@ -0,0 +1,12 @@
+package com.pysun.flutter
+
+class MavenExtension {
+    boolean useRemote = false
+    String remoteUrl
+    String userName
+    String password
+    String pomArtifactId
+    String pomGroupId
+    private String pomVersion
+    String localUrl
+}
---
+++ src/main/groovy/com/pysun/flutter/PublishFlutterAarPlugin.groovy
@@ -0,0 +1,65 @@
+package com.pysun.flutter
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PublishFlutterAarPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+        def mavenExt = project.extensions.create("mavenSupport", MavenExtension)
+        project.gradle.projectsEvaluated {
+
+            project.rootProject.subprojects.findAll { it.name != "app" }.each { subProject ->
+                subProject.tasks.findByName("uploadArchives").doFirst {
+                    assert subProject.hasProperty("output-dir")
+                    if (mavenExt.useRemote) {
+
+                        File outRepoDir = new File("${subProject.property("output-dir")}${File.separator}outputs${File.separator}repo")
+                        if (!outRepoDir.exists()) {
+                            outRepoDir.mkdirs()
+                        }
+                        subProject.uploadArchives.repositories.mavenDeployer.repository(url: mavenExt.remoteUrl) {
+                            authentication(userName: mavenExt.userName, password: mavenExt.password)
+                        }
+                    } else {
+
+                        if (mavenExt.localUrl?.trim()) {
+                            File outLocalDir = new File("${mavenExt.localUrl}")
+                            if (!outLocalDir.exists()) {
+                                outLocalDir.mkdirs()
+                            }
+                            subProject.uploadArchives.repositories.mavenDeployer.repository(url: mavenExt.localUrl)
+
+                        }
+
+
+                    }
+
+
+                    if (subProject.name.equalsIgnoreCase("flutter")) {
+                        subProject.uploadArchives.repositories.mavenDeployer.pom {
+                            artifactId = (subProject.uploadArchives.repositories.mavenDeployer.pom.artifactId).replace("flutter", mavenExt.pomArtifactId ?: "flutter")
+                            groupId = mavenExt.pomGroupId?:subProject.uploadArchives.repositories.mavenDeployer.pom.groupId
+
+//                            version = mavenExt.pomVersion ?: subProject.uploadArchives.repositories.mavenDeployer.pom.version
+
+                        }
+
+                    }
+
+                    //暂时无法弄清如何修改依赖插件版本
+//                    subProject.uploadArchives.repositories.mavenDeployer.pom {
+//                        version = mavenExt.pomVersion ?: subProject.uploadArchives.repositories.mavenDeployer.pom.version
+//
+//                    }
+
+
+                }
+            }
+        }
+
+
+    }
+}
